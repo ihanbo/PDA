f3b5795577f3cf61a918dd0d25e68b211b2edafe
Hanbo <hanbov5@gmail.com>
2016-12-18 00:42:37 +0800

    no message

--- app/src/main/java/com/abit/han/pda/bmob/BmobCenter.java
+++ app/src/main/java/com/abit/han/pda/bmob/BmobCenter.java
@@ -1,6 +1,9 @@
 package com.abit.han.pda.bmob;
 
 import android.app.Application;
+
+import com.abit.han.pda.util.Costant;
+
 import cn.bmob.v3.Bmob;
 
 /**
@@ -8,8 +11,8 @@ import cn.bmob.v3.Bmob;
  */
 
 public class BmobCenter {
-    public static final String BMOB_APP_KEY = "5767c5911dd06e44a0e7b2ed1fd334c7";
-    public static final String BMOB_API_KEY = "1db1ca1c134259300170f3b76e7e89fa";
+    public static final String BMOB_APP_KEY = Costant.Bmob.BMOB_APP_KEY;
+    public static final String BMOB_API_KEY = Costant.Bmob.BMOB_API_KEY;
 
 
     public static void init(Application app) {
