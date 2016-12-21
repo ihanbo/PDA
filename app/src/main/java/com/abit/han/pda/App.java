bf516cf04e4955a40d001cb274bc8593f18e857c
hanbo <hanbo1@bitauto.com>
2016-12-21 17:53:36 +0800

    no message

--- app/src/main/java/com/abit/han/pda/App.java
+++ app/src/main/java/com/abit/han/pda/App.java
@@ -1,8 +1,11 @@
 package com.abit.han.pda;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.abit.han.pda.bmob.BmobCenter;
 import com.abit.han.pda.recievepush.RecievePushCenter;
@@ -17,12 +20,17 @@ import com.xiaomi.mipush.sdk.MiPushCommandMessage;
 import com.xiaomi.mipush.sdk.MiPushMessage;
 import com.xiaomi.mipush.sdk.PushMessageReceiver;
 import com.xiaomi.mipushdemo.MiPushRecieverCenter;
+import com.xiaomi.mipushdemo.RecieveMiPushHelper;
 import com.yiche.library.ylog.ErrorListener;
 import com.yiche.library.ylog.Printer;
 import com.yiche.library.ylog.YLog;
 import com.yiche.net.NetCenter;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by ihanb on 2016/12/15.
@@ -53,37 +61,7 @@ public class App extends Application {
         BaseService.startService(IserviceData.START_SERVICE);
         //注册推送
         SendPushCenter.mInstance.initSendPush(this);
-        MiPushRecieverCenter.getInstance().register(this, new PushMessageReceiver() {
-            @Override
-            public void onReceivePassThroughMessage(Context context, MiPushMessage miPushMessage) {
-                super.onReceivePassThroughMessage(context, miPushMessage);
-            }
-
-            @Override
-            public void onNotificationMessageClicked(Context context, MiPushMessage miPushMessage) {
-                super.onNotificationMessageClicked(context, miPushMessage);
-            }
-
-            @Override
-            public void onNotificationMessageArrived(Context context, MiPushMessage miPushMessage) {
-                super.onNotificationMessageArrived(context, miPushMessage);
-            }
-
-            @Override
-            public void onReceiveMessage(Context context, MiPushMessage miPushMessage) {
-                super.onReceiveMessage(context, miPushMessage);
-            }
-
-            @Override
-            public void onReceiveRegisterResult(Context context, MiPushCommandMessage miPushCommandMessage) {
-                super.onReceiveRegisterResult(context, miPushCommandMessage);
-            }
-
-            @Override
-            public void onCommandResult(Context context, MiPushCommandMessage miPushCommandMessage) {
-                super.onCommandResult(context, miPushCommandMessage);
-            }
-        });
+        MiPushRecieverCenter.getInstance().register(this, miPushReciever);
         RecievePushCenter.mInstance.initRecievePush(this);
         //ServiceDispatch注册服务
         ServiceDispatch.registeAllServices();
@@ -132,5 +110,65 @@ public class App extends Application {
                     }
                 });
     }
+
+
+    private PushMessageReceiver miPushReciever = new PushMessageReceiver() {
+        @Override
+        public void onReceivePassThroughMessage(Context context, MiPushMessage miPushMessage) {
+            super.onReceivePassThroughMessage(context, miPushMessage);
+        }
+
+        @Override
+        public void onNotificationMessageClicked(Context context, MiPushMessage miPushMessage) {
+            super.onNotificationMessageClicked(context, miPushMessage);
+        }
+
+        @Override
+        public void onNotificationMessageArrived(Context context, MiPushMessage message) {
+            String log = context.getString(com.xiaomi.mipushdemo.R.string.arrive_notification_message, new Object[]{message.getContent()});
+            RecieveMiPushHelper.logList.add(0, getSimpleDate() + " " + log);
+            String type = "no type!";
+            if(!TextUtils.isEmpty(message.getTopic())) {
+                type = "topic:"+message.getTopic();
+            } else if(!TextUtils.isEmpty(message.getAlias())) {
+                type = "alias:"+message.getAlias();
+            }
+            YLog.i(type+"  "+log);
+        }
+
+        @Override
+        public void onReceiveMessage(Context context, MiPushMessage miPushMessage) {
+            super.onReceiveMessage(context, miPushMessage);
+        }
+
+        @Override
+        public void onReceiveRegisterResult(Context context, MiPushCommandMessage message) {
+            String command = message.getCommand();
+            List arguments = message.getCommandArguments();
+            String cmdArg1 = arguments != null && arguments.size() > 0?(String)arguments.get(0):null;
+            String log;
+            if("register".equals(command)) {
+                if(message.getResultCode() == 0L) {
+                    String mRegId = cmdArg1;
+                    log = "mRegId:"+mRegId+"  "+context.getString(com.xiaomi.mipushdemo.R.string.register_success);
+                } else {
+                    log = context.getString(com.xiaomi.mipushdemo.R.string.register_fail);
+                }
+            } else {
+                log = message.getReason();
+            }
+            YLog.i(log);
+        }
+
+        @Override
+        public void onCommandResult(Context context, MiPushCommandMessage miPushCommandMessage) {
+            super.onCommandResult(context, miPushCommandMessage);
+        }
+
+        @SuppressLint({"SimpleDateFormat"})
+        private  String getSimpleDate() {
+            return (new SimpleDateFormat("MM-dd hh:mm:ss")).format(new Date());
+        }
+    };
 
 }
