23686dfaef803f890b1d0cfc91700ac9b8a1d03d
hanbo <hanbo1@bitauto.com>
2016-12-21 17:31:16 +0800

    no message

---
+++ app/src/inhome/java/com/abit/han/pda/flavors/DiffFlavor.java
@@ -0,0 +1,10 @@
+package com.abit.han.pda.flavors;
+
+/**
+ * 不同Flavor下的数据
+ */
+
+public class DiffFlavor {
+
+    public static final String MI_PUUSH_ACCOUNT = "woshimimax";
+}
---
+++ app/src/outhome/java/com/abit/han/pda/flavors/DiffFlavor.java
@@ -0,0 +1,9 @@
+package com.abit.han.pda.flavors;
+
+/**
+ * Created by ihanb on 2016/12/21.
+ */
+
+public class DiffFlavor {
+    public static final String MI_PUUSH_ACCOUNT = "woshimi5";
+}
